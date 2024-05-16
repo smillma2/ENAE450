bc506793365361a0ddc4cc807c849177e6d92ee7
Nandini Ramachandran <98720566+nani-r@users.noreply.github.com>
2024-05-16 11:03:49 -0400

    updated code for hardware, used on competition run

--- maze_simulation/maze_simulation/maze_sim.py
+++ maze_simulation/maze_simulation/maze_sim.py
@@ -9,63 +9,78 @@ class SolveMaze(Node):
     def __init__(self):
         super().__init__('turtle_follower')
         
-        # distance threshold
-        self.threshold = 0.4
+        # front and side distance thresholds; if robot rear corner is hitting wall when turning, consider increasing side threshold.
+        # if robot rear corner is hitting wall when "done" with turn and adjusting to be proper distance again, consider increasing fron threshold
+        self.front_threshold = 0.25
+        self.side_threshold = 0.29
         
         self.front = 999
         self.e_dist_l = 999
-        self.e_ang_l = 0
         self.e_dist_r = 999
         self.e_ang_r = 0
-        self.data = [0]
 
         # initialization of publisher
         self.cmdvel_publisher = self.create_publisher(Twist, '/cmd_vel', 10)
         
         # initialization of main timer
-        self.timer = self.create_timer(0.1, self.timer_callback)
+        self.timer = self.create_timer(0.2, self.timer_callback)
         
         # initialization of scan subscription
         self.scan_subscriber = self.create_subscription(Scan, '/scan', self.get_scan_data, 10)
 
     # get relevant data from lidar scan
     def get_scan_data(self, data):
-        self.data = data.ranges
-        self.front = min(data.ranges[360:380] + data.ranges[340:360])
+        # ON REAL ROBOT, CHANGE TO 360 instead of 0
+        # self.front = data.ranges[0]
+        self.front = min(data.ranges[320:380])
         
-        data_left = [12.0 if x == float('inf') else x for x in data.ranges[450:630]] #45:135
-        data_right = [12.0 if x == float('inf') else x for x in data.ranges[90:270]] #225:315
+        # ON REAL ROBOT, CHANGE TO PROPER DATA RANGES
+        data_left = [12.0 if x == float('inf') else x for x in data.ranges[450:630]]
+        data_right = [12.0 if x == float('inf') else x for x in data.ranges[90:270]]
+
         
         self.e_dist_l = min(data_left)
         self.e_dist_r = min(data_right)
-
-        #self.get_logger().info('e_dist_l: %s' % str(self.e_dist_l))
-        #self.get_logger().info('e_dist_r: %s' % str(self.e_dist_r))
         
-        self.e_ang_l = data.ranges.index(self.e_dist_l)
         self.e_ang_r = data.ranges.index(self.e_dist_r)
-
+        
+        #self.get_logger().info('Chunk 4: %s' % str(chunk_4))
+    
     # main control loop
     def timer_callback(self):
         message = Twist()
-        self.get_logger().info("Min Index: %s" % str(self.data.index(min(self.data))))
-
-        aim_angle = (self.e_ang_l + self.e_ang_r)/2
         
-        wall_dist_modifier = (self.e_dist_l - self.e_dist_r) / ((self.e_dist_l + self.e_dist_r)/2)
+        # ON REAL ROBOT, CHANGE 270 to RIGHTMOST ANGLE (540? idk)
+        # try to align w/ rightmost angle
+        aim_angle = self.e_ang_r - 180
         
-        if self.front < self.threshold:
-            message.linear.x = 0.0
-            message.angular.z = 0.1 if self.e_dist_l > self.e_dist_r else -0.1 
-            self.get_logger().info('Front: %s' % str(self.front))
-        else:
-            message.linear.x = 2.0
-            message.angular.z = 0.3
-            #message.angular.z = (aim_angle * (2*(math.pi/180)) + wall_dist_modifier)
-            #self.get_logger().info('Wall Mod: %s' % str((aim_angle * math.pi/180) + wall_dist_modifier))
+        # attempt to stay a specific distance away from right wall, unless left wall is too close, in which case stay centered between left and right
+        wall_dist_parameter = self.side_threshold if self.side_threshold < self.e_dist_l else self.e_dist_l
+        
+        # calculate how much to modify turn to align w/ walls
+        wall_dist_modifier = (wall_dist_parameter - self.e_dist_r) / ((wall_dist_parameter + self.e_dist_r)/2)
+        
+        # this case should only trigger if it's time to turn left at an inner corner to keep following the right wall
+        if self.front < self.front_threshold:
+            # add hysteresis so that robot makes a smooth turn instead of turning a bit, moving forward a bit, and repeating
+            self.front_threshold = 0.6
             
-        # log current state
-        #self.get_logger().info(self.state)
+            # sit and turn instead of moving forward
+            message.linear.x = 0.0
+            message.angular.z = 0.2
+        # otherwise, just follow wall (this may include making outside turns)
+        # if self.e_dist_r > self.side_threshold:
+        #     message.linear.x = 0.0
+        #     message.angular.z = -0.2
+        else:
+            # reset front threshold after hysteresis, if it happened
+            self.front_threshold = 0.34
+            # move forward at constant rate
+            message.linear.x = 0.17
+            
+            # ON REAL ROBOT, CHANGE 180 to 360 (i think, maybe test to make sure) TO ADJUST FOR DOUBLED ANGLE RANGE
+            # turn specific amount to stay the correct distance from the right wall (or in between right and left if they're close)
+            message.angular.z = (aim_angle * math.pi/360) + wall_dist_modifier + 0.09
         
         # send commands to robot
         self.cmdvel_publisher.publish(message)
--- maze_simulation/maze_simulation/maze_sim_2.py
+++ maze_simulation/maze_simulation/maze_sim_2.py
@@ -17,159 +17,38 @@ class SolveMaze(Node):
 
         # Subscription initialization
         self.scan_subscriber = self.create_subscription(Scan, '/scan', self.get_scan_data, 10)
-        
-        # states = ["finding wall", "turning_right_start", "turning_right_end", "turning_left_start", "turning_left_end"]
-        self.state = "finding_wall"
-        self.turn_counter = 10
-        
-        # storage for the previous angle sums while turning
-        self.previous_angle_sums = []
-        
+                
         self.data = None
         
 
     def get_scan_data(self, data):
         # Extract distances at specific angles
         self.data = data.ranges
+        self.data = [x if x != float('inf') else 12 for x in self.data]
         
-    def is_right_wall_contiguous(self, right_data):
-        right_data = [12 if x == float('inf') else x for x in right_data]
-        # Initialize a counter for persistent jumps
-        persistent_jump_count = 0
-        
-        # Iterate over the data starting from the second element
-        for i in range(1, len(right_data)):
-            # Check if there is a significant jump from the previous data point
-            if right_data[i] - right_data[i-1 - persistent_jump_count] > 1:
-                # Increment the jump counter
-                persistent_jump_count += 1
-                # If the jump has persisted for at least 4 indices, return the start index of the first jump
-                if persistent_jump_count >= 3:
-                    return i - persistent_jump_count
-            else:
-                # Reset the counter if the jump does not persist
-                persistent_jump_count = 0
-                
-        # Return -1 if no persistent significant jump is found
-        return -1
-    
-    def get_right_wall_angle_with_interp(self, right_data):
-        # interpolate out the infs
-        right_data[0] = 6 if right_data[0] == float('inf') else right_data[0]
-        for i in range(1, len(right_data)):
-            if right_data[i] == float('inf'):
-                right_data[i] = right_data[i-1]
-                
-        clipped_right_data = [[]]
-        for i in range(1, len(right_data)):
-            if right_data[i] - right_data[i-1] > 0.5:
-                clipped_right_data.append([])
-            clipped_right_data[-1].append(right_data[i])
-            
-        # get longest sublist
-        clipped_right_data = max(clipped_right_data, key=len)
-                
-        angle_sum = 0
-        
-        for _ in range(30):
-            i = random.randint(0, len(clipped_right_data)//2 - 1)
-            angle_sum += clipped_right_data[i] - clipped_right_data[len(clipped_right_data) - i - 1]
-        
-        return angle_sum / 10
-    
-    def get_right_wall_angle(self, right_data):
-        right_data = [12 if x == float('inf') else x for x in right_data]
-        # randomly choose a number i from 0 to len(right_data)/2
-        # sample the data right_data[i] and right_data[len(right_data) - i]
-        # sum up the differences between the two samples and return that sum
-        angle_sum = 0
-        for _ in range(30):
-            i = random.randint(0, len(right_data)//2 - 1)
-            angle_sum += right_data[i] - right_data[len(right_data) - i - 1]
-        
-        return angle_sum / 30
-    
-    def linearize_right_wall(self, right_data):
-        right_data[0] = 6 if right_data[0] == float('inf') else right_data[0]
-        for i in range(1, len(right_data)):
-            if right_data[i] == float('inf'):
-                right_data[i] = right_data[i-1]
-        center_index = len(right_data) // 2
-        
-        linearized_data = []
-        
-        for i in range(len(right_data)):
-            angle = (i - center_index) / 2
-            linearized_data.append(right_data[i] * math.cos(math.radians(angle)))
-            
-        return linearized_data
-    
-    def get_right_wall_variance(self, right_data):
-        
-        linearized_data = self.linearize_right_wall(right_data)
-
-        self.get_logger().info("linearized %s" % str(linearized_data))
-        
-        return sum([(x - sum(linearized_data)/len(linearized_data))**2 for x in linearized_data]) / len(linearized_data)
-        
+       
     def timer_callback(self):
         message = Twist()
         
-        linear_x = 0.0
+        linear_x = 0.3
         angular_z = 0.0
-        
-        if self.state == "finding_wall":
-            # look for wall in front of the robot
-            if self.data:
-                forward_data = self.data[345:375]
-                right_data = self.data[90:270]
-                minimum_distance = min(forward_data)
-                if minimum_distance < 0.6:
-                    self.state = "turning_left_start"
-                    self.turn_counter = 10
-                else:
-                    contiguous_result = self.is_right_wall_contiguous(right_data)
-                    # # if the right wall is contiguous, keep moving forward
-                    # if contiguous_result != -1 and contiguous_result < 10:
-                    #     self.state = "turning_right_start"
-                    #     self.turn_counter = 10
-                    # else:
-                    linear_x = 0.5
-                    angular_z = self.get_right_wall_angle_with_interp(right_data)
-                        
-        if self.state == "turning_right_start":
-            if self.turn_counter > 0:
-                linear_x = 0.0
-                angular_z = -0.5
-                self.turn_counter -= 1
-            else:
-                self.state = "turning_right_end"
-        
-        if self.state == "turning_left_start":
-            if self.turn_counter > 0:
-                linear_x = 0.0
-                angular_z = 0.5
-                self.turn_counter -= 1
-                self.previous_angle_sums.append(abs(self.get_right_wall_angle(self.data[90:270])))
-            else:
-                self.state = "turning_left_end"
-                
-        if self.state == "turning_left_end":
-            linear_x = 0.0
-            angular_z = 0.5
-            self.previous_angle_sums.append(self.get_right_wall_angle(self.data[90:270]))
-            variance = self.get_right_wall_variance(self.data[90:270])
-            self.get_logger().info("variance %s" % str(variance))
-            self.get_logger().info("angle %s" % str(self.previous_angle_sums[-1]))
-            # check if the signs of the last 3 elements in previous_angle_sums are different from the sign
-            # of the sum of the last 10 elements in previous_angle_sums
-            are_last_three_signs_same = all([x > 0 for x in self.previous_angle_sums[-3:]]) or all([x < 0 for x in self.previous_angle_sums[-3:]])
-            last_10_sum = sum(self.previous_angle_sums[-10:])
-            if are_last_three_signs_same and last_10_sum * self.previous_angle_sums[-1] < 0 and variance < 0.1:
-                self.state = "finding_wall"
-                self.previous_angle_sums = []
 
-            
+        # Find closest point (index) from indices 0 to 360
+        # if the closest point's index is below 180, turn right
+        # otherwise turn left
+        # Turn proportionally to how far the index is from 180
+        if self.data:
+            min_pos = self.data[:360].index(min(self.data[:360]))
+            angular_z = (180 - min_pos)/360
+
+            self.get_logger().info("min index: %s" % str(min_pos))
+            self.get_logger().info("min val %s" % str(min(self.data[:360])))
+            self.get_logger().info("angular val %s" % str(angular_z))
+        # Add a threshold check for basically all around the robot
+        # if you are too close just rotate, no linear
+        # ggs
+        if self.data and min(self.data[:360]) < 0.45:
+            linear_x = 0.0
 
         message.linear.x = linear_x
         message.angular.z = angular_z
